6d66a9e856dd29f0d3e77ca9e380fd8c6ecf619a
USER <fullstackmachinelearning@gmail.com>
2020-10-10 23:32:09 +0900

    init

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import * as firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCZEq3ONQn8PlOTBRd6osXuFAmE1G9Yi4Q",
+  authDomain: "nwitter-4f518.firebaseapp.com",
+  databaseURL: "https://nwitter-4f518.firebaseio.com",
+  projectId: "nwitter-4f518",
+  storageBucket: "nwitter-4f518.appspot.com",
+  messagingSenderId: "687855056255",
+  appId: "1:687855056255:web:cc6211f4511aa688cbd64a",
+  measurementId: "G-41VVY69LHZ",
+};
+
+export default firebase.initializeApp(firebaseConfig);
